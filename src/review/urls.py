ee2c0bb100b2d40e7831bd3fc29084441614ae1c
Jesse London <jesselondon@gmail.com>
2018-02-15 17:29:06 -0600

    review reviews & look up applications (#37)
    Dashboard from which reviewer may look up applications to review and review their submitted reviews.
    
    * enable review of *requested* (rather than auto-selected) application, and even re-review (modifying submitted review)
    
    part of #11
    
    * added more useful str representations of models and added a common filter to the review queryset
    
    * added index/home page, listing reviewer's submitted reviews, with links to update them
    
    part of #11 and #20
    
    * added application look-up form to dashboard
    
    part of #20
    resolves #29

--- src/review/urls.py
+++ src/review/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path, re_path
-from django.views.generic import RedirectView
 
 from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
+
     path('review/application/', views.review, name='review-application'),
+    path('review/application/<int:application_id>/', views.review, name='review-application'),
+
     # path('review/interview/', views.review, name='review-interview'),
     # path('review/interview/...', views.review, name='review-interview-detail'),
 
-    # TODO: do something with index? (dashboard?)
-    path('', RedirectView.as_view(pattern_name='review-application', permanent=False)),
+    path('application.json', views.list_applications, {'content_type': 'json'}, name='application-list'),
+    # path('application/', views.list_applications, name='application-list'),
 
     re_path(r"confirm-email/(?P<key>[-:\w]+)/$",
             views.invite_confirm_email,
--- src/review/views.py
+++ src/review/views.py
@@ -9,11 +9,11 @@ from django import forms, http
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db import transaction
+from django.db import connection, transaction
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_GET, require_http_methods
 
 from review import models, query
 
@@ -92,48 +92,143 @@ def unexpected_review(handler):
     return functools.wraps(handler)(wrapped)
 
 
+@require_GET
+@login_required
+def index(request):
+    reviews = request.user.reviews.current_year()
+    review_count = len(reviews)  # let's get them
+
+    if review_count == 0:
+        return redirect('review-application')
+
+    return TemplateResponse(request, 'review/index.html', {
+        'reviews': reviews,
+        'review_count': review_count,
+        'program_year': settings.REVIEW_PROGRAM_YEAR,
+    })
+
+
+@require_GET
+@login_required
+def list_applications(request, content_type='html'):
+    if content_type != 'json':
+        raise NotImplementedError
+
+    query_raw = request.GET.get('q', '')
+    applications = query.apps_to_review(request.user,
+                                        include_reviewed=True,
+                                        ordered=False)
+
+    if query_raw:
+        with connection.cursor() as cursor:
+            cursor.execute('''\
+                SELECT "EntryId"
+                FROM "survey_application_1_2018"
+                WHERE
+                    LOWER("Field451") IN %(query_terms)s OR
+                    LOWER("Field452") IN %(query_terms)s OR
+                    LOWER("Field461") IN %(query_terms)s
+                ''',
+                {'query_terms': tuple(query_raw.lower().split())}
+            )
+            entry_ids = [row[0] for row in cursor]
+
+        applications = applications.filter(
+            applicationpage__table_name='survey_application_1_2018',
+            applicationpage__column_name='EntryId',
+            applicationpage__entity_code__in=entry_ids,
+        )
+    elif not request.user.trusted:
+        return http.JsonResponse(
+            {
+                'status': 'forbidden',
+                'error': 'not allowed',
+            },
+            status=403,
+        )
+
+    return http.JsonResponse({
+        'status': 'ok',
+        'results': list(
+            applications
+            .values(
+                'application_id',
+                'applicant_id',
+                'program_year',
+                'created',
+                'applicant__email',
+            )
+            .order_by(
+                'applicant__email',
+            )
+        ),
+    })
+
+
 @require_http_methods(['GET', 'POST'])
 @login_required
 @unexpected_review
-def review(request):
-    if request.method == 'POST':
-        application_id = request.POST.get('application', '')
-
-        if not application_id.isdigit():
-            return http.HttpResponseBadRequest("Bad request")
-
+def review(request, application_id=None):
+    if application_id:
         applications = query.apps_to_review(request.user,
-                                            application_id=application_id)
-
+                                            application_id=application_id,
+                                            include_reviewed=True)
         try:
             (application,) = applications
         except ValueError:
-            # application *may* exist but it is not in the set of those
-            # allowed to reviewer
-            return http.HttpResponseForbidden("Forbidden")
+            return http.HttpResponseNotFound("Could not find application to review")
+
+        try:
+            review = application.review_set.get(reviewer=request.user)
+        except models.Review.DoesNotExist:
+            review = None
+    else:
+        review = None
+
+    if request.method == 'POST':
+        if application_id is None:
+            application_id = request.POST.get('application', '')
+
+            if not application_id.isdigit():
+                return http.HttpResponseBadRequest("Bad request")
+
+            applications = query.apps_to_review(request.user,
+                                                application_id=application_id)
+
+            try:
+                (application,) = applications
+            except ValueError:
+                # application *may* exist but it is not in the set of those
+                # allowed to reviewer
+                return http.HttpResponseForbidden("Forbidden")
+        elif request.POST.get('application') != str(application_id):
+            return http.HttpResponseBadRequest("Bad request")
 
         review_form = ReviewForm(data=request.POST,
+                                 instance=review,
                                  reviewer=request.user)
         if review_form.is_valid():
             with transaction.atomic():
                 review_form.save()
 
             messages.success(request, 'Review submitted')
-            return redirect('review-application')
+            return redirect(request.path)
     else:
-        applications = query.apps_to_review(request.user, limit=1)
+        if application_id is None:
+            applications = query.apps_to_review(request.user, limit=1)
 
-        try:
-            (application,) = applications
-        except ValueError:
-            return TemplateResponse(request, 'review/noapps.html', {
-                'program_year': settings.REVIEW_PROGRAM_YEAR,
-                'review_count': request.user.reviews.filter(
-                    application__program_year=settings.REVIEW_PROGRAM_YEAR
-                ).count(),
-            })
+            try:
+                (application,) = applications
+            except ValueError:
+                return TemplateResponse(request, 'review/noapps.html', {
+                    'program_year': settings.REVIEW_PROGRAM_YEAR,
+                    'review_count': request.user.reviews.filter(
+                        application__program_year=settings.REVIEW_PROGRAM_YEAR
+                    ).count(),
+                })
 
-        review_form = ReviewForm(initial={'application': application},
+        review_form = ReviewForm(instance=review,
+                                 initial={'application': application},
                                  reviewer=request.user)
 
     return TemplateResponse(request, 'review/review.html', {

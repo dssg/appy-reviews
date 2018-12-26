49fbaf1ad1a3ce24d51cf3abee399daf0f1610b0
Jesse London <jesselondon@gmail.com>
2018-12-26 13:40:23 -0600

    management commands (#52)
    * management commands, to generate the static closed.html &amp; upload it to the S3 bucket, and to manage the DNS CName to point to either CloudFront or Beanstalk
    
    ...plus deferred updates because `argcmdr` was in flux ;)
    
    * more clean-up of management file against new argcmdr features

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
-import argparse
+import contextlib
+import enum
 import os
+import sys
+from argparse import REMAINDER
 from pathlib import Path
 
-from argcmdr import Local, LocalRoot
-from descriptors import cachedclassproperty
+from argcmdr import Local, LocalRoot, localmethod
+from descriptors import cachedproperty
+from plumbum import colors
 
 
 ROOT_PATH = Path(__file__).parent.resolve()
@@ -33,10 +37,21 @@ class Build(Local):
             help=f'Image name/tag (default: {self.DEFAULT_NAMETAG})',
         )
         parser.add_argument(
+            '--label',
+            action='append',
+            help='Additional name/tags to label image; the first of these, '
+                 'if any, is treated as the "version"',
+        )
+        parser.add_argument(
             '--target',
             choices=('development', 'production'),
             default='production',
             help="Target environment (default: production)",
+        )
+        parser.add_argument(
+            '-l', '--login',
+            action='store_true',
+            help="log in to AWS ECR",
         )
         parser.add_argument(
             '-p', '--push',
@@ -48,67 +63,72 @@ class Build(Local):
             action='store_true',
             help="deploy the container once the image is pushed",
         )
-        parser.add_argument(
-            '-l', '--login',
-            action='store_true',
-            help="log in to AWS ECR",
-        )
 
-    def prepare(self, args):
+    def prepare(self, args, parser):
         if args.login and not args.push:
-            args.__parser__.error("will not log in outside of push operation")
+            parser.error("will not log in outside of push operation")
 
-        yield self.local['docker'][
+        command = self.local['docker'][
             'build',
             '--build-arg', f'TARGET={args.target}',
             '-t', args.name,
             '-t', self.get_full_name(args.name),
-            ROOT_PATH,
         ]
 
+        if args.label:
+            for label in args.label:
+                command = command['-t', label]
+
+        yield command[ROOT_PATH]
+
         if args.push:
-            yield from args.__children__.push.prepare(args)
+            yield from self['push'].prepare(args)
 
         if args.deploy:
-            yield args.__children__.deploy.prepare(args)
+            yield self['deploy'].prepare(args)
 
-    class Push(Local):
+    @localmethod('-l', '--login', action='store_true', help="log in to AWS ECR")
+    def push(self, args):
         """push already-built image to registry"""
-
-        def __init__(self, parser):
-            parser.add_argument(
-                '-l', '--login',
-                action='store_true',
-                help="log in to AWS ECR",
-            )
-
-        def prepare(self, args):
-            if args.login:
-                login_command = self.local['aws'][
-                    'ecr',
-                    'get-login',
-                    '--no-include-email',
-                    '--region', 'us-west-2',
-                ]
-                if args.show_commands:
-                    print('>', login_command)
-                if args.execute_commands:
-                    full_command = login_command()
-                    (executable, *arguments) = full_command.split()
-                    assert executable == 'docker'
-                    yield self.local[executable][arguments]
-
-            yield self.local['docker'][
-                'push',
-                Build.get_full_name(args.name),
+        if args.login:
+            login_command = self.local['aws'][
+                'ecr',
+                'get-login',
+                '--no-include-email',
+                '--region', 'us-west-2',
             ]
+            if args.show_commands or not args.execute_commands:
+                print('>', login_command)
+            if args.execute_commands:
+                full_command = login_command()
+                (executable, *arguments) = full_command.split()
+                assert executable == 'docker'
+                yield self.local[executable][arguments]
 
-    class Deploy(Local):
+        yield self.local['docker'][
+            'push',
+            self.get_full_name(args.name),
+        ]
+
+    @localmethod
+    def deploy(self, args):
         """deploy an image container"""
+        command = self.local['eb']['deploy']
 
-        def prepare(self, args):
-            return self.local['eb']['deploy']
+        # specify environment
+        if args.target == 'production':
+            command = command['appy-reviews-pro']
+        else:
+            command = command['appy-reviews-dev']
 
+        if args.label:
+            return command['-l', args.label[0]]
+        return command
+
+
+#                                                 #
+# Base classes for management of Docker container #
+#                                                 #
 
 class LocalContainer(Local):
 
@@ -134,30 +154,35 @@ class DbLocal(LocalContainer):
 
     EXAMPLE_DATABASE_URL = 'postgres://appy_reviews:PASSWORD@localhost:5433/appy_reviews'
 
-    @classmethod
-    def get_database_url(cls, args):
-        if args.database_url:
-            return args.database_url
+    @cachedproperty
+    def database_url(self):
+        if self.args.database_url:
+            return self.args.database_url
 
         if os.getenv('DATABASE_URL'):
             return None
 
-        args.__parser__.error(
+        self.args.__parser__.error(
             'DATABASE_URL must be specified by argument or environment\n\n'
             'For example:\n'
-            f'\tmanage db --database-url={cls.EXAMPLE_DATABASE_URL}\n'
+            f'\tmanage db --database-url={self.EXAMPLE_DATABASE_URL}\n'
             'or:\n'
-            f'\tDATABASE_URL={cls.EXAMPLE_DATABASE_URL} manage db'
+            f'\tDATABASE_URL={self.EXAMPLE_DATABASE_URL} manage db'
         )
 
-    @classmethod
-    def manage(cls, args, **kwargs):
-        return cls.run(
-            DATABASE_URL=cls.get_database_url(args),
+    def manage(self, *args, **kwargs):
+        return self.run(
+            *args,
+            DATABASE_URL=self.database_url,
             **kwargs
         )['./manage.py']
 
     def __init__(self, parser):
+        parser.add_argument(
+            '-v', '--verbosity',
+            type=int,
+            default=1,
+        )
         parser.add_argument(
             '--db', '--database-url',
             dest='database_url',
@@ -173,6 +198,7 @@ class Develop(DbLocal):
 
     """
     DEFAULT_NAMETAG = 'appyreviews_web_1'
+    CONTROLS = ('start', 'stop', 'restart')
 
     def __init__(self, parser):
         super().__init__(parser)
@@ -200,51 +226,34 @@ class Develop(DbLocal):
         yield self.run(
             '-d',
             '--name', args.name,
-            DATABASE_URL=self.get_database_url(args),
+            DATABASE_URL=self.database_url,
             APPY_DEBUG=1,
+            SMTP_USER=None,
+            SMTP_PASSWORD=None,
         )
 
-    class Control(Local):
+    @localmethod('mcmd', metavar='command', choices=CONTROLS,
+                 help="{{{}}}".format(', '.join(CONTROLS)))
+    def control(self, args):
         """control the appy-reviews supervisor process"""
+        return self.local['docker'][
+            'exec',
+            args.name,
+            'supervisorctl',
+            '-c',
+            '/etc/supervisor/supervisord.conf',
+            args.mcmd,
+            'webapp',
+        ]
 
-        commands = ('start', 'stop', 'restart')
-
-        def __init__(self, parser):
-            parser.add_argument(
-                'mcmd',
-                metavar='command',
-                choices=self.commands,
-                help="{{{}}}".format(', '.join(self.commands)),
-            )
-
-        def prepare(self, args):
-            return self.local['docker'][
-                'exec',
-                args.name,
-                'supervisorctl',
-                '-c',
-                '/etc/supervisor/supervisord.conf',
-                args.mcmd,
-                'webapp',
-            ]
-
-    class DjManage(Local):
+    @localmethod('mcmd', metavar='command', help="django management command")
+    @localmethod('remainder', metavar='command arguments', nargs=REMAINDER)
+    def djmanage(self, args):
         """manage the appy-reviews django project"""
-
-        def __init__(self, parser):
-            parser.add_argument(
-                'mcmd',
-                metavar='command',
-                help="django management command",
-            )
-            parser.add_argument(
-                'remainder',
-                metavar='command arguments',
-                nargs=argparse.REMAINDER,
-            )
-
-        def prepare(self, args):
-            return self.local['docker'][
+        return (
+            # foreground command to fully support shell
+            self.local.FG,
+            self.local['docker'][
                 'exec',
                 '-it',
                 args.name,
@@ -252,6 +261,7 @@ class Develop(DbLocal):
                 args.mcmd,
                 args.remainder,
             ]
+        )
 
 
 @Appy.register
@@ -260,6 +270,7 @@ class Db(DbLocal):
 
     commands = (
         'showmigrations',
+        'makemigrations',
         'migrate',
     )
 
@@ -275,53 +286,196 @@ class Db(DbLocal):
         parser.add_argument(
             'remainder',
             metavar='command arguments',
-            nargs=argparse.REMAINDER,
+            nargs=REMAINDER,
         )
 
     def prepare(self, args):
-        return self.manage(args)[args.mcmd, args.remainder]
+        return self.manage('-it')[args.mcmd, args.remainder]
 
 
 @Appy.register
 class Etl(DbLocal):
     """manage survey data"""
 
-    class Apps(Local):
+    @localmethod('subcommand',
+                 choices=('execute', 'inspect',), default='execute', nargs='?',
+                 help="either execute command, or inspect state of system "
+                      "only, do not load applications (default: execute)")
+    def apps(self, args):
         """map wufoo data into appy"""
+        return self.manage()[
+            'loadapps',
+            '-s', '_2018',
+            '2018',
+            args.subcommand,
+        ]
 
-        def prepare(self, args):
-            return Etl.manage(args)[
-                'loadapps',
-                '-s', '_2018',
-                '-y', '2018',
-            ]
-
-    class Wufoo(Local):
+    @localmethod('--csv', action='store_true', default=False, dest='csv_cache',
+                 help="cache data in local CSV files")
+    def wufoo(self, args, parser):
         """load initial survey data"""
-
-        def __init__(self, parser):
-            parser.add_argument(
-                '--csv',
-                action='store_true',
-                default=False,
-                dest='csv_cache',
-                help="cache data in local CSV files",
+        if not os.getenv('WUFOO_API_KEY'):
+            parser.error(
+                'WUFOO_API_KEY must be specified by environment\n\n'
+                'For example:\n'
+                '\tWUFOO_API_KEY=xx-xx-xx DATABASE_URL=... manage etl bootstrap'
             )
 
-        def prepare(self, args):
-            if not os.getenv('WUFOO_API_KEY'):
-                args.__parser__.error(
-                    'WUFOO_API_KEY must be specified by environment\n\n'
-                    'For example:\n'
-                    '\tWUFOO_API_KEY=xx-xx-xx DATABASE_URL=... manage etl bootstrap'
+        # FIXME: This wasn't working, rather only when command is run
+        # FIXME: by user in shell
+        # TODO: Likely must disable TEE modifier and use, say, FG, instead
+        return self.manage(WUFOO_API_KEY=None)[
+            'loadwufoo',
+            '-v', args.verbosity,
+            '-f', '"^2018 "',
+            ('output' if args.csv_cache else '-'),
+        ]
+
+
+@Appy.register
+class Reviewer(DbLocal):
+    """manage reviewers"""
+
+    @localmethod('remainder', metavar='command arguments', nargs='*',
+                 help="arguments to pass to sendconfirm (preceeded by --)")
+    def invite(self, args):
+        """invite a new or existing reviewer"""
+        return self.manage(
+            SMTP_USER=None,
+            SMTP_PASSWORD=None,
+        )[
+            'sendinvite',
+            args.remainder,
+        ]
+
+
+@Appy.register
+class Static(LocalContainer):
+    """generate static pages"""
+
+    @localmethod
+    def closed(self):
+        """render closed.html and upload it to the S3 bucket as index.html"""
+        return (
+            # render to stdout
+            self.run()[
+                './manage.py',
+                'makestatic',
+                'closed',
+                'program_year={settings.REVIEW_PROGRAM_YEAR}',
+            ] |
+            # pipe rendering to awscli
+            # and upload to bucket
+            self.local['aws'][
+                's3',
+                'cp',
+                '-',
+                's3://review.dssg.io/index.html',
+                '--acl', 'public-read',
+                '--content-type', 'text/html',
+            ]
+        )
+
+
+@Appy.register
+class DNS(Local):
+    """manage site DNS"""
+
+    class StrEnum(str, enum.Enum):
+
+        def __str__(self):
+            return self.value
+
+    class Domain(StrEnum):
+
+        zone = 'dssg.io.'
+        fqdn = f'review.{zone}'
+
+    class CName(StrEnum):
+
+        live = 'pro-reviews-dssg.us-west-2.elasticbeanstalk.com'
+        static = 'd2va83k0l3phq8.cloudfront.net'
+
+    class profile_hint(contextlib.AbstractContextManager):
+        """Print a helpful hint about using the correct AWS profile in
+        the event of an execution error.
+
+        Note that the exception is not suppressed.
+
+        """
+        def __exit__(self, _exc_type, exc_value, _exc_tb):
+            if isinstance(exc_value, Local.local.ProcessExecutionError):
+                print(
+                    'Hint: specify your AWS profile for the DSSG account:\n'
+                    '\n'
+                    '\tAWS_PROFILE=dssg manage dns ...\n',
+                    file=sys.stderr,
                 )
 
-            return Etl.manage(
-                args,
-                WUFOO_API_KEY=None,
-            )[
-                'loadwufoo',
-                '-f', '"^2018 "',
-                '-s', '2018-stream',
-                ('output' if args.csv_cache else '-'),
+    @classmethod
+    def prepare_hosted_zone(cls):
+        # AWS_PROFILE=dssg
+        return cls.local['aws'][
+            'route53',
+            'list-hosted-zones',
+            '--query', f"HostedZones[?Name == '{cls.Domain.zone}']",
+        ] | cls.local['jq'][
+            '.[0].Id',
+        ]
+
+    @localmethod
+    def check(self):
+        """look up the site's current DNS setting"""
+        with self.profile_hint():
+            (_retcode, output, _error) = yield (
+                self.prepare_hosted_zone() |
+                self.local['xargs'][
+                    '-I', '{}',
+                    'aws',
+                    'route53',
+                    'list-resource-record-sets',
+                    '--hosted-zone-id', '{}',
+                    '--query', f"ResourceRecordSets[?Name == '{self.Domain.fqdn}']",
+                ] | self.local['jq'][
+                    '-r',
+                    '.[0].ResourceRecords[0].Value',
+                ]
+            )
+
+        if output is None:
+            # dry run
+            return
+
+        cname = output.strip()
+        for value in self.CName:
+            if value == cname:
+                print(value.name.upper() | colors.info)
+                break
+        else:
+            print('UNKNOWN' | colors.warn)
+
+    @localmethod('target', choices=CName.__members__, help="the CNAME value to set")
+    def set(self, args):
+        """set the site's DNS to either the live server or the static
+        content bucket
+
+        """
+        with self.profile_hint():
+            yield self.prepare_hosted_zone() | self.local['xargs'][
+                '-I', '{}',
+                'aws',
+                'route53',
+                'change-resource-record-sets',
+                '--hosted-zone-id', '{}',
+                '--change-batch', '''{
+                    "Changes": [{
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": "%s",
+                            "Type": "CNAME",
+                            "TTL": 300,
+                            "ResourceRecords": [{"Value": "%s"}]
+                        }
+                    }]
+                }''' % (self.Domain.fqdn, self.CName[args.target]),
             ]

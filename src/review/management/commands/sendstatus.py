35568a830377ebb88159730ff67eb957bc3be3aa
Jesse London <jesselondon@gmail.com>
2020-01-29 00:45:39 -0600

    made sendstatus command by default remind recommenders with unsubmitted references regardless of application status
    and added guard against duplicate emails (regardless of bad input data)

--- src/review/management/commands/sendstatus.py
+++ src/review/management/commands/sendstatus.py
@@ -1,4 +1,5 @@
 import itertools
+import sys
 from collections import namedtuple
 
 from django.conf import settings
@@ -12,7 +13,8 @@ from .base import UnbrandedEmailCommand
 
 class Command(UnbrandedEmailCommand):
 
-    help = "Send final status emails to applicants and reminders to unsubmitted references"
+    help = ("Send final status emails (to applicants -- unimplemented -- and) "
+            "reminders to unsubmitted references")
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -38,34 +40,69 @@ class Command(UnbrandedEmailCommand):
         parser.add_argument(
             '--incomplete',
             action='store_true',
+            dest='opt_incomplete',
             help="notify applicants who did not complete their applications",
         )
         parser.add_argument(
             '--unsubmitted',
             action='store_true',
+            dest='opt_unsubmitted',
             help="notify applicants with completed applications but less than "
                  "two references",
         )
         parser.add_argument(
             '--complete',
             action='store_true',
+            dest='opt_complete',
             help="notify applicants with completed applications and at least "
                  "two references",
         )
         parser.add_argument(
             '--all-applicants',
             action='store_true',
+            dest='opt_all_applicants',
             help="notify all applicants of their statuses",
         )
 
         parser.add_argument(
             '--references',
             action='store_true',
+            dest='opt_references',
             help="notify recommenders who haven't submitted references for "
-                 "applicants with completed applications",
+                 "applicants (regardless of application completion: see "
+                 "--references-complete)",
+        )
+        parser.add_argument(
+            '--references-complete',
+            action='store_true',
+            dest='opt_references_complete',
+            help="do NOT email recommender if their applicant hasn't yet "
+                 "completed their application",
+        )
+        parser.add_argument(
+            '--references-template',
+            dest='opt_references_template',
+            default='review/email/reference_reminder',
+            metavar='PATH',
+            help="reference email template prefix "
+                 "(default: review/email/reference_reminder)",
         )
 
     def send_mail(self, template, address, context, on_behalf=None):
+        # It's *possible* to send duplicate emails (most commonly due to bad,
+        # but unavoidable, user data). Try to catch these.
+        email_signature = (template, address.lower())  # assumes context unvariable
+        if on_behalf:
+            email_signature += (on_behalf.lower(),)
+
+        if email_signature in self._references_emailed:
+            print("[WARN] duplicate email (will not send)",
+                  "to:", address, "for:", on_behalf or '<none>',
+                  file=sys.stderr)
+            return
+
+        self._references_emailed.add(email_signature)
+
         action_text = 'emailed' if self._send_mail else 'would-email'
         log_args = (action_text, address)
         if on_behalf:
@@ -79,68 +116,74 @@ class Command(UnbrandedEmailCommand):
             # print(on_behalf, address)  # TODO: perhaps add --debug-log option to output like this
             print(*log_args)
 
-    def handle(self, incomplete, unsubmitted, complete, all_applicants, references, send_mail,
-               test_emails, debug_sql, verbosity, **_opts):
+    def handle(self, opt_incomplete, opt_unsubmitted, opt_complete, opt_all_applicants,
+               opt_references, opt_references_complete, opt_references_template,
+               send_mail, test_emails, debug_sql, verbosity, **_opts):
         self._send_mail = send_mail
         self._verbosity = verbosity
+
+        self._references_emailed = set()
 
         if debug_sql:
             print(sql_statement())
             return
 
-        if incomplete or unsubmitted or complete or all_applicants:
+        if opt_incomplete or opt_unsubmitted or opt_complete or opt_all_applicants:
             # This is intended largely for them; but, Rayid handled this, this year
             raise NotImplementedError
 
-        if all_applicants and (incomplete or unsubmitted or complete):
+        if opt_all_applicants and (opt_incomplete or opt_unsubmitted or opt_complete):
             raise CommandError("redundant argumentation")
 
-        if all_applicants:
-            incomplete = unsubmitted = complete = True
+        if opt_all_applicants:
+            opt_incomplete = opt_unsubmitted = opt_complete = True
 
-        if not incomplete and not unsubmitted and not complete and not references:
+        if not opt_incomplete and not opt_unsubmitted and not opt_complete and not opt_references:
             raise CommandError("nothing to do")
 
         results = stream_test(test_emails) if test_emails else stream_applications()
         for result in results:
             if not result.app_completed:
-                if incomplete:
+                if opt_incomplete:
                     raise NotImplementedError
 
-                # nothing more to do with incomplete applications
-                continue
+                if opt_references_complete:
+                    # nothing more to do with incomplete applications
+                    continue
 
             references_submitted = (result.ref0_submitted, result.ref1_submitted)
-            if not all(references_submitted):
-                if references:
-                    app_references = (result[3:6], result[6:9])
-                    reference_link = REFERENCE_FORM_URL.format(app_email=result.app_email)
+            if not all(references_submitted) and opt_references and (result.app_completed or
+                                                                     not opt_references_complete):
+                app_references = (result[3:6], result[6:9])
+                reference_link = REFERENCE_FORM_URL.format(app_email=result.app_email)
 
-                    for ((ref_first,
-                          ref_last,
-                          ref_email),
-                         reference_submitted) in zip(app_references, references_submitted):
-                        if not reference_submitted:
-                            self.send_mail(
-                                'review/email/reference_status',
-                                ref_email,
-                                {
-                                    'applicant_first': result.app_first,
-                                    'applicant_last': result.app_last,
-                                    'ref_first': ref_first,
-                                    'ref_last': ref_last,
-                                    'reference_link': mark_safe(reference_link),
-                                    'program_year': settings.REVIEW_PROGRAM_YEAR,
-                                },
-                                on_behalf=result.app_email,
-                            )
+                for ((ref_first,
+                      ref_last,
+                      ref_email),
+                     reference_submitted) in zip(app_references, references_submitted):
+                    if not reference_submitted:
+                        self.send_mail(
+                            opt_references_template,
+                            ref_email,
+                            {
+                                'applicant_first': result.app_first,
+                                'applicant_last': result.app_last,
+                                'ref_first': ref_first,
+                                'ref_last': ref_last,
+                                'target_first': ref_first,
+                                'target_last': ref_last,
+                                'reference_link': mark_safe(reference_link),
+                                'program_year': settings.REVIEW_PROGRAM_YEAR,
+                            },
+                            on_behalf=result.app_email,
+                        )
 
             if len(result.references) < 2:
-                if unsubmitted:
+                if opt_unsubmitted:
                     raise NotImplementedError
 
             else:
-                if complete:
+                if opt_complete:
                     raise NotImplementedError
 
 

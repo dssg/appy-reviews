34d8c97c9fe85d8066a551259a3abd1585238be3
Jesse London <jesselondon@gmail.com>
2022-01-15 15:54:45 -0600

    "sendstatus --complete" to send applicants confirmation email + db record
    implementation of "--complete" flag of "sendstatus" command to send
    confirmation emails to applicants whose applications are complete and
    whose recommendations have been receieved.
    
    record of confirmation's transmission persisted to new table s.t. email
    will not be sent again.

--- src/review/management/commands/sendstatus.py
+++ src/review/management/commands/sendstatus.py
@@ -1,11 +1,13 @@
 import itertools
 import urllib
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 
 from django.conf import settings
 from django.core.management.base import CommandError
-from django.db import connection
+from django.db import connection, IntegrityError
 from django.utils.safestring import mark_safe
+
+from review import models
 
 from . import APPLICANT_SURVEY_FIELDS, REFERENCE_SURVEY_FIELDS, REFERENCE_FORM_URL
 from .base import UnbrandedEmailCommand, exhaust_iterable
@@ -55,7 +57,7 @@ class Command(UnbrandedEmailCommand):
             action='store_true',
             dest='opt_complete',
             help="notify applicants with completed applications and at least "
-                 "two references (UNIMPLEMENTED)",
+                 "two references",
         )
         parser.add_argument(
             '--all-applicants',
@@ -99,13 +101,14 @@ class Command(UnbrandedEmailCommand):
     def handle(self, opt_incomplete, opt_unsubmitted, opt_complete, opt_all_applicants,
                opt_references, opt_references_complete, opt_references_template,
                send_mail, test_emails, debug_sql, verbosity, **_opts):
+        self._create_cache = defaultdict(list)
         self._verbosity = verbosity
 
         if debug_sql:
             print(sql_statement())
             return
 
-        if opt_incomplete or opt_complete or opt_all_applicants:
+        if opt_incomplete or opt_all_applicants:
             # This is intended largely for them; but, Rayid handled these, this year
             raise NotImplementedError
 
@@ -129,7 +132,20 @@ class Command(UnbrandedEmailCommand):
 
         if send_mail:
             send_count = self.send_batched_email(messages)
-            self.stdout.write(f'[INFO] sent {send_count}')
+            self.stderr.write(f'I: sent {send_count}')
+
+            for (model, objs) in self._create_cache.items():
+                if objs:
+                    try:
+                        model.objects.bulk_create(objs, ignore_conflicts=True)
+                    except IntegrityError as exc:
+                        batch_ids = [obj.application_id for obj in objs]
+                        self.stderr.write(
+                            f'E: integrity error affecting batch {model.__name__} '
+                            f'application_id={batch_ids}: {exc}'
+                        )
+                    else:
+                        self.stderr.write(f'I: inserted {len(objs)} ({model._meta.db_table})')
         else:
             exhaust_iterable(messages)
 
@@ -145,7 +161,7 @@ class Command(UnbrandedEmailCommand):
 
             if email_signature in references_emailed:
                 self.stderr.write(
-                    f"[WARN] duplicate email (will not send) to: {address} for: " +
+                    f"W: duplicate email (will not send) to: {address} for: " +
                     (on_behalf or '<none>')
                 )
                 continue
@@ -181,7 +197,7 @@ class Command(UnbrandedEmailCommand):
             references_submitted = (status.ref0_submitted, status.ref1_submitted)
             if not all(references_submitted) and opt_references and (status.app_completed or
                                                                      not opt_references_complete):
-                app_references = (status[3:6], status[6:9])
+                app_references = (status[4:7], status[7:10])
 
                 for ((ref_first,
                       ref_last,
@@ -219,9 +235,24 @@ class Command(UnbrandedEmailCommand):
                         None,
                     )
 
-            else:
-                if opt_complete:
-                    raise NotImplementedError
+            elif opt_complete and status.app_completed and not status.email_complete_sent:
+                if status.application_id is None:
+                    self.stderr.write(f'E: application not found: {status.app_email}')
+                else:
+                    self._create_cache[models.ApplicationCompleteMessage].append(
+                        models.ApplicationCompleteMessage(application_id=status.application_id)
+                    )
+
+                    yield (
+                        'review/email/applicant_complete',
+                        status.app_email,
+                        {
+                            'applicant_first': status.app_first,
+                            'applicant_last': status.app_last,
+                            'program_year': settings.REVIEW_PROGRAM_YEAR,
+                        },
+                        None,
+                    )
 
 
 def sql_statement():
@@ -243,13 +274,15 @@ def sql_statement():
 
     return f"""\
         WITH application_status AS (
-                SELECT DISTINCT {select_fields}, (
+                SELECT DISTINCT page.application_id, {select_fields}, (
                     CASE WHEN survey_2."{app_email}" IS NULL THEN false ELSE true END
                 ) as app_completed, (
                     CASE WHEN rec_j0."{ref_email}" IS NULL THEN false ELSE true END
                 ) as ref0_submitted, (
                     CASE WHEN rec_j1."{ref_email}" IS NULL THEN false ELSE true END
-                ) as ref1_submitted
+                ) as ref1_submitted, (
+                    CASE WHEN email_complete.email_message_id IS NULL THEN false ELSE true END
+                ) as email_complete_sent
                 FROM survey_application_1_{settings.REVIEW_PROGRAM_YEAR} AS survey_1
                 LEFT OUTER JOIN survey_application_2_{settings.REVIEW_PROGRAM_YEAR} AS survey_2 ON(
                     LOWER(survey_1."{app_email}") = LOWER(survey_2."{app_email}")
@@ -262,6 +295,12 @@ def sql_statement():
                     LOWER(survey_1."{ref1_email}") = LOWER(rec_j1."{ref_email}") AND
                     LOWER(survey_1."{app_email}") = LOWER(rec_j1."{app_email}")
                 )
+                LEFT OUTER JOIN application_page page ON (
+                    page.table_name = 'survey_application_1_{settings.REVIEW_PROGRAM_YEAR}' AND
+                    page.column_name = 'EntryId' AND
+                    page.entity_code = survey_1."EntryId"
+                )
+                LEFT OUTER JOIN email_message_application_complete email_complete USING (application_id)
             ),
             application_reference AS (
                 SELECT DISTINCT ON (LOWER("{app_email}"), LOWER("{ref_email}"))
@@ -287,11 +326,13 @@ def sql_statement():
 
 ApplicationStatus = namedtuple(
     'ApplicationStatus',
+    ['application_id'] +
     [label for (label, _col_name) in APPLICANT_SURVEY_FIELDS] +
     [
         'app_completed',
         'ref0_submitted',
         'ref1_submitted',
+        'email_complete_sent',
         'references',
     ]
 )
@@ -319,6 +360,7 @@ def stream_test(emails):
             reference1 = [f'Reference1 {count} First', f'Reference1 {count} Last', ref1_email]
 
             yield ApplicationStatus(
+                0,
                 f'Applicant {count} First',
                 f'Applicant {count} Last',
                 app_email,
@@ -331,6 +373,7 @@ def stream_test(emails):
                 app_completed,
                 ref0_submitted,
                 ref1_submitted,
+                False,
                 [
                     ref for (ref, ref_submitted) in (
                         (reference0, ref0_submitted),
